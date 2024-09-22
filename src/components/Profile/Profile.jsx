e39a4f6b11f502bab30a9f74db3e7095dfaf6d5c
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-09-22 22:58:58 +0300

    Add Profile markup

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,28 +1,29 @@
-import styles from './Profile.module.css';
-
-const Profile = ({ name, tag, location, image, stats }) => (
-  <div>
+const Profile = ({ name, tag, location, image, stats }) => {
+  return (
     <div>
-      <img className={styles.image} src={image} alt="User avatar" />
-      <p>{name}</p>
-      <p>{tag}</p>
-      <p>{location}</p>
-    </div>
+      <div>
+        <img src={image} alt="User avatar" />
+        <p>{name}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </div>
 
-    <ul>
-      <li>
-        <span>Followers</span>
-        <span>{stats}</span>
-      </li>
-      <li>
-        <span>Views</span>
-        <span>{stats}</span>
-      </li>
-      <li>
-        <span>Likes</span>
-        <span>{stats}</span>
-      </li>
-    </ul>
-  </div>
-);
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
 export default Profile;
