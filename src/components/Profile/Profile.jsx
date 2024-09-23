811e018e1686cabbe10a504493bc8de35882bf5d
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-09-23 20:46:14 +0300

    Fix bugs

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,30 +2,28 @@ import styles from './Profile.module.css';
 
 const Profile = ({ name, tag, location, image, stats }) => {
   return (
-    <div className={styles.container}>
-      <div className={styles.profile}>
-        <div className={styles.description}>
-          <img className={styles.avatar} src={image} alt="User avatar" />
-          <p className={styles.username}>{name}</p>
-          <p className={styles.tag}>@{tag}</p>
-          <p className={styles.location}>{location}</p>
-        </div>
-
-        <ul className={styles.stats}>
-          <li className={styles.item}>
-            <span>Followers</span>
-            <span className={styles.quantity}>{stats.followers}</span>
-          </li>
-          <li className={styles.item}>
-            <span>Views</span>
-            <span className={styles.quantity}>{stats.views}</span>
-          </li>
-          <li className={styles.item}>
-            <span>Likes</span>
-            <span className={styles.quantity}>{stats.likes}</span>
-          </li>
-        </ul>
+    <div className={styles.profile}>
+      <div className={styles.description}>
+        <img className={styles.avatar} src={image} alt="User avatar" />
+        <p className={styles.username}>{name}</p>
+        <p className={styles.tag}>@{tag}</p>
+        <p className={styles.location}>{location}</p>
       </div>
+
+      <ul className={styles.stats}>
+        <li className={styles.item}>
+          <span>Followers</span>
+          <span className={styles.quantity}>{stats.followers}</span>
+        </li>
+        <li className={styles.item}>
+          <span>Views</span>
+          <span className={styles.quantity}>{stats.views}</span>
+        </li>
+        <li className={styles.item}>
+          <span>Likes</span>
+          <span className={styles.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
     </div>
   );
 };
